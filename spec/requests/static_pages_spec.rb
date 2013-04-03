2bfb899fc99674657d095bcbe40b33766774b233
bon27 <jingkai64@gmail.com>
2013-04-03 08:50:41 +0800

    refactor the static_pages_spec test, dry out the base title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  let(:base_title){"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page"  do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
@@ -10,7 +12,7 @@ describe "Static Pages" do
 	it 'should have the right title' do
 	  visit '/static_pages/home'
 	  page.should have_selector('title',
-	                :text=>"Ruby on Rails Tutorial Sample App | Home")
+	                :text=>"#{base_title} | Home")
 	  end
   end
 
@@ -23,7 +25,7 @@ describe "Static Pages" do
     it "should have the title 'Help'" do
 	  visit '/static_pages/help'
 	  page.should have_selector("title",
-	                :text=>"Ruby on Rails Tutorial Sample App | Help")
+	                :text=>"#{base_title} | Help")
     end
   end
 
@@ -36,7 +38,7 @@ describe "Static Pages" do
 	it "should have the right title 'About Us'" do
 	  visit '/static_pages/about'
 	  page.should have_selector("title",
-	                :text=>"Ruby on Rails Tutorial Sample App | About Us")
+	                :text=>"#{base_title} | About Us")
 	  end
   end
 
@@ -49,7 +51,7 @@ describe "Static Pages" do
 	it "should have the right title 'Contact Us'" do
 	  visit '/static_pages/contact'
 	  page.should have_selector('title',
-	              :text=>"Ruby on Rails Tutorial Sample App | Contact Us")
+	              :text=>"#{base_title} | Contact Us")
     end
   end
 end
