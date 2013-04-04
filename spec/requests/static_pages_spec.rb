a82dc68d0123ffebbe5adab53a2a58b9cdcb252f
bon27 <jingkai64@gmail.com>
2013-04-04 09:15:14 +0800

    the book is right. just since I typed "static" as "satic" in the test rspec. passed test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,7 +13,7 @@ describe "Static Pages" do
     let(:heading) {'Sample App'}
 	let(:page_title) {''}
 
-	it {should_behave_like "all_static_pages"}
+	it_should_behave_like "all static pages"
 	it {should_not have_selector 'title', text: '| Home'}
   end
 
@@ -21,20 +21,22 @@ describe "Static Pages" do
 	  before {visit help_path}
 	  let(:heading) {'Help'}
 	  let(:page_title) {'Help'}
-      it {should_behave_like "all_static_pages"}
+      it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before {visit about_path}
     let(:heading) {"About Us"}
-	let(:pae_title) {"About Us"}
-	it {should_behave_like "all_satic_pages"}
+	let(:page_title) {"About Us"}
+	
+	it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
 	  before {visit contact_path}
 	  let(:heading) {"Contact Us"}
 	  let(:page_title) {"Contact Us"}
-     it {should_behave_like "all_static_pages"}
+      
+	  it_should_behave_like "all static pages"
   end
 end
