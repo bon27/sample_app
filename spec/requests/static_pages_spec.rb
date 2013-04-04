3f1d6469cf8bb8ac9a3fac4efc509dc8d64553b8
bon27 <jingkai64@gmail.com>
2013-04-04 08:55:42 +0800

    use shared_example to refactor the static pages test. yet one line in the books "it_should_hebave_like" is changed to "it {should_behave_like} to make the test valid"

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,29 +2,39 @@ require 'spec_helper'
 
 describe "Static Pages" do
   subject {page}
+  
+  shared_examples_for "all static pages" do
+    it {should have_selector("h1",text:heading)}
+	it {should have_selector("title",text:full_title(page_title))}
+  end
 
   describe "Home page"  do
 	before(:each){visit root_path}
-	it {  should have_selector("h1",:text=>"Sample App")}
-	it {  should have_selector('title',text: full_title(''))}
-	it {  should_not have_selector('title',:text=>'| Home')}
+    let(:heading) {'Sample App'}
+	let(:page_title) {''}
+
+	it {should_behave_like "all_static_pages"}
+	it {should_not have_selector 'title', text: '| Home'}
   end
 
   describe 'Help page' do
 	  before {visit help_path}
-	  it { should have_selector("h1",text: 'Help')}
-	  it { should have_selector("title",text: full_title('Help'))}
+	  let(:heading) {'Help'}
+	  let(:page_title) {'Help'}
+      it {should_behave_like "all_static_pages"}
   end
 
   describe "About page" do
     before {visit about_path}
-	  it { should have_selector('h1',:text=> "About Us")}
-	  it { should have_selector("title", :text=>full_title("About Us"))}
+    let(:heading) {"About Us"}
+	let(:pae_title) {"About Us"}
+	it {should_behave_like "all_satic_pages"}
   end
 
   describe "Contact page" do
 	  before {visit contact_path}
-	  it { should have_selector('h1',:text=>"Contact Us")}
-	  it { should have_selector('title',:text=>full_title("Contact Us"))}
+	  let(:heading) {"Contact Us"}
+	  let(:page_title) {"Contact Us"}
+     it {should_behave_like "all_static_pages"}
   end
 end
