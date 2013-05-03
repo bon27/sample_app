158baf3faf5868c491d54a2831c372cb5fb30c00
bon27 <jingkai64@gmail.com>
2013-05-03 16:00:33 +0800

    finished 9.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  
   def show
     @user = User.find(params[:id])
   end
@@ -16,6 +15,21 @@ class UsersController < ApplicationController
        redirect_to @user # redirect_to @user is shortcuted as: redirect_to @user
 	else
 		render 'new'
+	end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+	@user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+		flash[:success] = "Profile updated"
+		sign_in @user
+		redirect_to @user
+	else
+		render 'edit'
 	end
   end
 end
